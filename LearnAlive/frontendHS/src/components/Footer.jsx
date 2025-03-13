ef4596347306a05075076c3430f4f3e746ef5584
WhatTheHJ <fhseh14@gmail.com>
2025-03-13 14:22:26 +0900

    test

--- LearnAlive/frontendHS/src/components/Footer.jsx
+++ LearnAlive/frontendHS/src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
       <footer className="footer">
         <p>© 2025 Learn-Alive</p>
-        <p>문의: 3조@abc.com</p> 
+        <p>문의: 3조@abc.com    11확인용메세지</p> 
       </footer>
     );
   };
-  
+
   export default Footer;
   
